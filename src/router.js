3d282e3755de396f5c43da6d7c6cdbd86ff18fe3
Kennedy <kennedymessias.vb@gmail.com>
2020-07-25 20:55:10 -0300

    router configs

--- src/router.js
+++ src/router.js
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
+import Main from './pages/Main.vue';
+import Impacts from './pages/TweetsImpacts.vue';
+import PastInformations from './components/PastInformations.vue';
+import Predictions from './components/Predictions.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +19,28 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About,
+  },
+  {
+    path: '/main',
+    name: 'main',
+    component: Main,
+  },
+  {
+    path: '/impacts/:name',
+    name: 'impacts',
+    component: Impacts,
+    children: [
+      {
+        path: '/:name/past',
+        component: PastInformations,
+        name: 'past',
+      },
+      {
+        path: '/:name/predictions',
+        component: Predictions,
+        name: 'predictions',
+      },
+    ],
   },
 ];
 
