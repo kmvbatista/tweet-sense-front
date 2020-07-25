1ddcebafb4df433941ff2a7e863cd7da257e59fb
Kennedy <kennedymessias.vb@gmail.com>
2020-07-25 20:52:49 -0300

    Revert "main-screen"
    This reverts commit 114ba50381e0074a319ee8823dc0ff64b2dc6aae.

--- src/router.js
+++ src/router.js
@@ -2,10 +2,6 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './pages/Home.vue';
 import About from './pages/About.vue';
-import Main from './pages/Main.vue';
-import Impacts from './pages/TweetsImpacts.vue';
-import PastInformations from './components/PastInformations.vue';
-import Predictions from './components/Predictions.vue';
 
 Vue.use(VueRouter);
 
@@ -19,28 +15,6 @@ const routes = [
     path: '/about',
     name: 'about',
     component: About,
-  },
-  {
-    path: '/main',
-    name: 'main',
-    component: Main,
-  },
-  {
-    path: '/impacts/:name',
-    name: 'impacts',
-    component: Impacts,
-    children: [
-      {
-        path: '/:name/past',
-        component: PastInformations,
-        name: 'past',
-      },
-      {
-        path: '/:name/predictions',
-        component: Predictions,
-        name: 'predictions',
-      },
-    ],
   },
 ];
 
