959815f51de67172ed2e6f1ab457f7f80e226e3d
Kennedy <kennedymessias.vb@gmail.com>
2020-07-25 11:36:28 -0300

    basic about page

---
+++ src/pages/About.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about-main">
+    <img class="detail left" src="@/assets/green-square.svg" alt="detail" />
+    <img class="detail right" src="@/assets/green-square.svg" alt="detail" />
+    <div class="central-text-box">
+      <h1 class="title">ABOUT US</h1>
+      <div class="horizontal-band"></div>
+      <h4
+        class="subtitle"
+      >Sentiment analysis on tweets of influential people and correlate with socio-economic impacts of those tweets across the globe and make predictions for future impact of tweets and comments by influential people</h4>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.about-main {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  background-color: var(--color-yellow);
+  position: fixed;
+}
+.title {
+  word-spacing: normal;
+  color: var(--color-green);
+}
+.subtitle {
+  font-size: 1.2em;
+  padding: 0 10px;
+  color: var(--color-green);
+  text-align: center;
+}
+.horizontal-band {
+  background-color: var(--color-green);
+}
+</style>
