8e51f1bac5302f1129b9dbce8fd8bd6a62a35bc6
Kennedy <kennedymessias.vb@gmail.com>
2020-07-26 11:06:48 -0300

    navigation and responsivity

---
+++ src/components/FirstHeader.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="nav-bar">
+    <router-link :to="{name: 'home'}" class="tab__link">
+      <div class="button" :class="{selected: selected==='home'}">Home</div>
+    </router-link>
+    <router-link :to="{name: 'about'}" class="tab__link">
+      <div class="button" :class="{selected: selected==='about'}">About</div>
+    </router-link>
+    <router-link :to="{name: 'choose-person'}" class="tab__link">
+      <div class="button" :class="{selected: selected==='choose-person'}">TweetLytics</div>
+    </router-link>
+  </nav>
+</template>
+<script>
+export default {
+  props: ["selected"],
+};
+</script>
+<style scoped>
+.nav-bar {
+  width: 100%;
+  background-color: white;
+  display: flex;
+  justify-content: center;
+}
+.button {
+  color: var(--color-green);
+  padding: 0.5em;
+  position: relative;
+}
+:link {
+  margin: 0.5em 1em;
+}
+.button:hover {
+  transform: scale(1.05);
+}
+.button::before {
+  content: "";
+  transform-origin: bottom;
+  transition: transform 0.3s, width 0.2s cubic-bezier(1, 0, 0, 1) 0.2s,
+    background-color 0.3s;
+  background-color: var(--color-green);
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 3px;
+  width: 3px;
+  transform: scaleY(0);
+}
+.button:hover::before,
+.selected::before {
+  width: 100%;
+  transform: scaleY(1);
+}
+</style>
---
+++ src/components/SecondHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <nav class="nav-bar">
+    <router-link :to="{name: 'home'}" class="tab__link">
+      <div class="button" :class="{selected: selected==='home'}">Home</div>
+    </router-link>
+    <router-link :to="{name: 'about'}" class="tab__link">
+      <div class="button" :class="{selected: selected==='about'}">About</div>
+    </router-link>
+    <router-link :to="{name: 'choose-person'}" class="tab__link">
+      <div class="button" :class="{selected: selected==='choose-person'}">TweetLytics</div>
+    </router-link>
+  </nav>
+</template>
+<script>
+export default {
+  props: ["selected"],
+};
+</script>
+<style scoped>
+.nav-bar {
+  width: 100%;
+  background-color: var(--color-green);
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2em;
+}
+.button {
+  color: white;
+  padding: 0.5em;
+  position: relative;
+}
+:link {
+  margin: 0.5em 1em;
+}
+.button:hover {
+  transform: scale(1.05);
+}
+.button::before {
+  content: "";
+  transform-origin: bottom;
+  transition: transform 0.3s, width 0.2s cubic-bezier(1, 0, 0, 1) 0.2s,
+    background-color 0.3s;
+  background-color: white;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  height: 3px;
+  width: 3px;
+  transform: scaleY(0);
+}
+.button:hover::before,
+.selected::before {
+  width: 100%;
+  transform: scaleY(1);
+}
+</style>
